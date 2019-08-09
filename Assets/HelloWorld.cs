df907e5e00c2093fe5e2172232a9097ef2677b1b
Simas <simonas.deimantavicius@mediapark.com>
2019-08-09 11:56:28 +0300

    Finalize number wizard

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
     // Use this for initialization
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("G'day mate, welcome to numba wiz-ud...");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
         Debug.Log("Tell me if your number is higher or lower than: " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
-        max = max + 1;
+        max += 1;
     }
 
     // Update is called once per frame
@@ -27,17 +36,24 @@ public class HelloWorld : MonoBehaviour
         {
             min = guess;
             guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than..." + guess);
+            nextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
             guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than..." + guess);
+            nextGuess();
+
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("I am a genius!");
+            StartGame();
         }
+    }
+
+    void nextGuess()
+    {
+        Debug.Log("Is it higher or lower than..." + guess);
     }
 }
